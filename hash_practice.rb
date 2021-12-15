cf66fad28547a1b545e333cbd1f2f116e9caf52d
Brylan Gannon <brylan.gannon112@gmail.com>
2021-12-14 21:20:40 -0700

    Add model for an animal

--- hash_practice.rb
+++ hash_practice.rb
@@ -6,3 +6,12 @@ car {
   miles: 125000,
   gas_is_low: true,
 }
+
+# model for an animal
+animal {
+  species: "Guinea pig",
+  breed: "Silky",
+  has_been_fed: true,
+  is_hungry: true,
+  amount_owned: 2
+}
