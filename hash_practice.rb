0ff69e19f5cdb003c8865b484f0b254da613a1e0
Brylan Gannon <brylan.gannon112@gmail.com>
2021-12-14 21:23:46 -0700

    Add model for a customer

--- hash_practice.rb
+++ hash_practice.rb
@@ -1,4 +1,4 @@
-# model for a car
+# model of a car
 car {
   model: "Ford",
   year: "1992",
@@ -7,11 +7,19 @@ car {
   gas_is_low: true,
 }
 
-# model for an animal
+# model of an animal
 animal {
   species: "Guinea pig",
   breed: "Silky",
   has_been_fed: true,
-  is_hungry: true,
+  is_healthy: true,
   amount_owned: 2
+}
+
+# model of a customer (at a bar)
+customer {
+  name: "Ronald",
+  age: 24,
+  id_present: true,
+  is_intoxicated: false,
 }
